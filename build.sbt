ee1533369c21f42d78b1aa267d2e3b7601fc2ab0
Dickon <dikonikon@gmail.com>
2015-01-10 18:03:22 +0000

    akka pieces

--- build.sbt
+++ build.sbt
@@ -10,5 +10,6 @@ resolvers += "Akka Release Repository" at "http://repo.akka.io/releases/"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.3.3",
   "com.typesafe.akka" %% "akka-slf4j" % "2.3.3",
+  "com.typesafe.akka" %% "akka-testkit" % "2.3.3",
   "org.specs2" % "specs2_2.10" % "1.14" % "test"
 )
--- src/main/scala/com/dikonikon/layer17/Startup.scala
+++ src/main/scala/com/dikonikon/layer17/Startup.scala
@@ -1,20 +1,36 @@
 package com.dikonikon.layer17
 
-import akka.actor.{ActorLogging, Actor}
+import akka.actor.{ActorSystem, Props, ActorLogging, Actor}
+import scala.concurrent.duration._
 
 class NodeRootActor extends Actor with ActorLogging {
 
   def receive = {
-    case AddIPToDNS => log.debug("received {0}", "nothing")
+    case x => log.debug("received {0}", "nothing")
+  }
+}
+
+class Main extends Actor {
+
+  val nodeRoot = context.actorOf(Props[NodeRootActor])
+
+  nodeRoot ! AddIPToDNS()
+
+  def receive = {
+    case x => println(s"received $x")
   }
 }
 
 
 object NodeStart {
   def main(args: Array[String]): Unit = {
-    val initialActor = classOf[NodeRootActor].getName
-
-    akka.Main.main(Array(initialActor))
+    println("starting Akka system")
+    implicit val system = ActorSystem("Test")
+    println("creating main actor")
+    val mainActor = system.actorOf(Props[NodeRootActor])
+    val probe = TestProbe()
+    mainActor ! AddIPToDNS()
+    system.awaitTermination(10.seconds)
   }
 }
 
